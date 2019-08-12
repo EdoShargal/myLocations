bcb297a65a1598c5ec532baa15cee2c98c2f9d55
edo shargal <edosh528@gmail.com>
2019-08-12 19:10:37 +0300

    start from last version removing the google map id key

---
+++ src/app/locations/locations.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
+
+import { LocationsListComponent } from './locations-list/locations-list.component';
+import { LocationsRoutingModule } from './locations-routing.module';
+import { LocationDetailComponent } from './location-detail/location-detail.component';
+import { LocalStorageService } from './services/localStorage.service';
+import { LocalDBService } from './services/localDB.service';
+import { LocationEditComponent } from './location-edit/location-edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    LocationsListComponent,
+    LocationDetailComponent,
+    LocationEditComponent
+  ],
+  exports: [
+  ],
+  imports: [
+    CommonModule,
+    LocationsRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    LocalStorageService,
+    LocalDBService
+  ]
+})
+export class LocationsModule { }
